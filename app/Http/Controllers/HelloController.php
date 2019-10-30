d114fcfc3485688a77b320b0a9de4ce20d60684a
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-30 16:59:29 +0900

    パラメータを設定
    ルートパラメータを利用するため

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     //
-    public function index() {
+    public function index($id='noname', $pass='unknown') {
 
       return <<<EOF
 <html>
@@ -21,6 +21,10 @@ h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0 -50px 0px; }
 <body>
   <h1>index</h1>
   <p>これは、helloコントローラのindexアクションです</p>
+  <ul>
+    <li>ID: {$id}</li>
+    <li>PASS: {$pass}</li>
+  </ul>
 </body>
 </html>
 EOF;
