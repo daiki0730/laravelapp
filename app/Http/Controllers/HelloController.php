d739314076b67b4202860cdfc7873ee385a3b887
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-10-30 16:47:50 +0900

    indexアクションを設定
    アクションメソッド利用してブラウザへ情報を送信

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,4 +7,22 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     //
+    public function index() {
+
+      return <<<EOF
+<html>
+<head>
+  <title>Hello</title>
+<style>
+body {font-size:16pt; color: #999;}
+h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0 -50px 0px; }
+</style>
+</head>
+<body>
+  <h1>index</h1>
+  <p>これは、helloコントローラのindexアクションです</p>
+</body>
+</html>
+EOF;
+    }
 }
