3f4a8a7dc35c63cfbba17d8dba9bea3bc43020b4
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-04 22:26:18 +0900

    不要な記述を削除

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -2,28 +2,3 @@
 
 @section ('title', 'Index')
 
-<html>
-<head>
-  <title>Hello/Index</title>
-  <style>
-  body {font-size:16pt; color: #999;}
-  h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0 -50px 0px; }
-  </style>
-</head>
-
-<body>
-  <h1>Blade/Index</h1>
-  <p>&#064;foreachディレクティブの例</p>
-  <ol>
-  @foreach ($data as $item)
-  @if ($loop->first)
-  <p>※データ一覧</p><ul>
-  @endif
-  <li>No,{{$loop->iteration}}.{{$item}}</li>
-  @if ($loop->last)
-  </ul><p>--ここまで</p>
-  @endif
-  @endforeach
-  </ol>
-</body>
-</html>
