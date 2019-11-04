7bd09c431de886d6e461a5583386f5691af3699d
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-04 23:42:10 +0900

    サブビューを利用
    @includeディレクティブを使用

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,15 +10,8 @@
 @section('content')
   <p>ここが本文のコンテンツです</p>
 
-  @component('components.message')
-  @slot('msg_title')
-  CAUTION!
-  @endslot
+  @include('components.message', ['msg_title'=>'OK', 'msg_content'=>'サブビュー'])
 
-  @slot('msg_content')
-  これはメッセージの表示です
-  @endslot
-  @endcomponent
 @endsection
 
 @section('footer')
