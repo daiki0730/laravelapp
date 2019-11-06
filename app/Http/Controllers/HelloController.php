78d029b4fcf130124d3a004dc13ddb8f95012c9b
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-06 23:25:23 +0900

    変数のデータを用意

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,7 +10,12 @@ class HelloController extends Controller {
 
   public function index()
   {
-    return view('hello.index');
+    $data = [
+      ['name'=>'山田一号','mail'=>'yamada@1'],
+      ['name'=>'山田二号','mail'=>'yamada@2'],
+      ['name'=>'山田三号','mail'=>'yamada@3']
+    ];
+    return view('hello.index', ['data'=>$data]);
   }
 
   public function post(Request $request)
