d13c75507c12c679e7c9905f513724de0bd7b167
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-07 19:18:22 +0900

    view_messageという値を設定
    /helloのindexビューと連携

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -10,6 +10,10 @@ class HelloServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        //
+        View::composer(
+            'hello.index',function($view){
+                $view->with('view_message','composer message!');
+            }
+        );
     }
 }
