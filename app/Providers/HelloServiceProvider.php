7f69645c695d7a3539b638d8826d070f5aa38644
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-07 19:10:51 +0900

    Registerを削除
    サービスの登録を行わないため

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -6,21 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class HelloServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot()
     {
         //
