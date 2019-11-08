65a19ab32a5d6d7ea0bf55af2d94827c7558bda0
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-11-08 15:48:16 +0900

    インデントを修正

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -1,9 +1,9 @@
 <?php
 
-  namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-  use Illuminate\Http\Request;
-  use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 
 class HelloController extends Controller {
